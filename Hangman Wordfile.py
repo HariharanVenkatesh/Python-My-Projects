f003a847bd496c1323f966ba49692c92ea0e2132
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-08-04 19:34:19 +0530

    Hangman Word File

---
+++ Hangman Wordfile.py
@@ -0,0 +1,8 @@
+Words_list = [
+    "apple", "banana", "carrot", "dog", "elephant", "fish", "guitar", "hat", "igloo", "jacket",
+    "kite", "lemon", "mango", "notebook", "orange", "piano", "quilt", "rabbit", "sweater", "table",
+    "umbrella", "violin", "watermelon", "xylophone", "yogurt", "zebra", "airplane", "ball", "cat",
+    "dinosaur", "eggplant", "flower", "giraffe", "hamburger", "ice cream", "jellyfish", "kangaroo",
+    "laptop", "moon", "ninja", "octopus", "penguin", "quokka", "rocket", "sun", "tiger", "unicorn",
+    "volcano", "whale", "xylophone", "yacht", "zeppelin"
+]
