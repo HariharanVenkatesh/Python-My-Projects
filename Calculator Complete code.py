fa354bf3df5d2bc35cf684582c9059024aab134d
HariharanVenkatesh <harihaniscristo23@gmail.com>
2024-08-04 19:22:26 +0530

    Calculator Complete Code

---
+++ Calculator Complete code.py
@@ -0,0 +1,39 @@
+#Project no 6 - Calculator complete code
+
+def add(a,b):
+    return a + b
+def subtract(a,b):
+    return a - b
+def multiply(a,b):
+    return a * b
+def divide(a,b):
+    return a / b
+operations_dict = {
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":divide
+}
+def calculator():
+    num1 = float(input("Enter First No: "))
+    for symbol in operations_dict:
+        print(symbol)
+    continue_flag = True
+    while continue_flag:
+        op_symbol = input("Pick an Operation: ")    
+        num2 = float(input("Enter Next No: "))
+        calculator_function = operations_dict[op_symbol]
+        output = calculator_function(num1,num2)
+        print(f"{num1} {op_symbol} {num2} = {output}")
+
+        should_continue = input(f"Enter 'y' to continue calculation with {output} or 'n' to start a new calculation or 'x' to exit: ").lower()
+        if should_continue == 'y':
+            num1 = output
+        elif should_continue == 'n':
+            continue_flag = False
+            os.system('cls')
+            calculator()
+        else:
+            continue_flag = False
+            print("Bye")
+calculator()                
